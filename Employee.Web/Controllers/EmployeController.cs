e0dd59127a0280ffd3ae0d6ed9e2c4550a246480
VIJAY NELSON <VIJAY NELSON@LAPTOP-CNA8QG75>
2024-06-25 06:14:47 +0530

    Add project files.

---
+++ Employee.Web/Controllers/EmployeController.cs
@@ -0,0 +1,46 @@
+﻿using Employee.Web.Data;
+using Employee.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Employee.Web.Controllers
+{
+    public class EmployeController : Controller
+    {
+        public EmployeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpGet]
+         public async Task<IActionResult> List()
+        {
+            var students = await DbContext.Studemts.ToListAsync()
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel viewModel) 
+        {
+            var emplyee = new employe
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Probation = viewModel.Probation
+
+
+            };
+
+          
+            await DbContext.Employe.AddAsync(emplyee)
+            await DbContext.SaveChangesAsync();
+
+            return View();
+        }
+}
---
+++ Employee.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Employee.Web.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee.Web.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Employe> Employes { get; set; }
+    }
+}
